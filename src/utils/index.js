9e8bd9041583a435c131f7415598317ab9562abf
Mauricio Contreras <virtual.kur2@gmail.com>
2020-02-12 22:50:50 +0100

    util for getUser on mention added

---
+++ src/utils/index.js
@@ -0,0 +1,17 @@
+const utils = {
+  getUserFromMention: (client, mention) => {
+    if (!mention) return;
+
+    if (mention.startsWith('<@') && mention.endsWith('>')) {
+      mention = mention.slice(2, -1);
+
+      if (mention.startsWith('!')) {
+        mention = mention.slice(1);
+      }
+
+      return client.users.get(mention);
+    }
+  }
+}
+
+module.exports = utils;
