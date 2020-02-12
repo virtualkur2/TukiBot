77908c8ab363dc365f439727a1ed07e1813ba89e
Mauricio Contreras <virtual.kur2@gmail.com>
2020-02-12 22:49:26 +0100

    slap command finished

--- src/commands/slap.js
+++ src/commands/slap.js
@@ -1,7 +1,22 @@
+const giphyClient = require('../giphyClient');
+const slapGifs = {q: 'slap', limit: 50};
 const slapCommand = {
   name: 'slap',
   description: 'Slap user',
-  execute(message, args) {
-    
+  execute(message, users) {
+    giphyClient.search('gifs', slapGifs)
+      .then(response => {
+        const totalResponses = response.data.length;
+        const randomIndex = Math.floor(Math.random()*totalResponses);
+        const randomGif = response.data[randomIndex];
+        let msg = users[0] ? (users[1] ? `Mielda ${users[1]}, tremenda cachetada te sampó ${users[0]}.` : `Agarra ahí ${users[0]}, sóbese.`) : 'Ja güeno.';
+        message.channel.send(msg, {
+          files: [randomGif.images.fixed_height.url]
+        });
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
   }
 }
+module.exports = slapCommand;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 require('dotenv').config();
 const { prefix, invoke } = require('../config.json');
+const utils = require('./utils');
 const botToken = process.env.DISCORD_BOT_TOKEN;
 
 const invokeBot = `${prefix}${invoke}`;
@@ -28,25 +29,39 @@ const invokedBy = [];
 const ignoreTime = 1000 * 60 * 5;
 
 client.on('message', message => {
-  console.log(message.author.bot);
   if(!message.content.startsWith(invokeBot)|| message.author.bot) return;
   if(message.content.length > invokeBot.length && message.content.charAt(invokeBot.length) !== ' ') return;
   
   // actions to do when bot invoked
-  let args = message.content.slice(prefix.length).split(/ +/);
-  args.shift();
+  let args = message.content.slice(invokeBot.length).split(' ');
+  //remove void char
+  if(!args[0]) args.shift();
+
   if(!args.length) {
      return client.commands.get('noargs').execute(message, invokedBy, ignoreTime);
   }
   const command = args.shift().toLowerCase();
-  if (command === 'ping') {
-    client.commands.get('ping').execute(message, args);
+  switch(command) {
+    case 'ping':
+      client.commands.get('ping').execute(message, args);
+      break;
+    case 'cachetada':
+      const user1 = utils.getUserFromMention(client, args[0]);
+      const user2 = utils.getUserFromMention(client, args[1]);
+      const users = [user1, user2];
+      client.commands.get('slap').execute(message, users);
+      break;
+    default:
+      return;
   }
+  // if (command === 'ping') {
+  //   client.commands.get('ping').execute(message, args);
+  // }
 });
           
 client.login(botToken)
   .then(() => {
-          
+
   })
   .catch(error => {
     console.error('An error occurred during login');
