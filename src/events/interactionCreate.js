d77f45e9c4237723f3ae8bedb02fefdbcbe7f4e7
Mauricio Contreras <virtual.kur2@gmail.com>
2023-02-21 11:28:22 +0100

    fix: export module
    add missing export on interaction create module
    
    refs: dev upgrade

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,22 +1,25 @@
 const { Events } = require('discord.js');
 
 const interactionCreate = {
-  name: Events.InteractionCreate,
-  async execute(interaction) {
-    if(!interaction.isChatInputCommand()) {
-      return;
-    }
-    const command = interaction.client.commands.get(interaction.commandName);
-    if(!command) {
-      console.error(`[ERROR] No se encuentra un comando llamado ${interaction.commandName}`);
-      return;
-    }
-    try {
-      console.log(`Comando recibido: ${interaction.commandName}`);
-      await command.execute(interaction);
-    } catch (error) {
-      console.error(error);
-      await interaction.reply({ content: 'Mielda menol, no entendí el beta!', ephemeral: true });
-    }
-  }
-}
+	name: Events.InteractionCreate,
+	async execute(interaction) {
+		if (!interaction.isChatInputCommand()) {
+			return;
+		}
+		const command = interaction.client.commands.get(interaction.commandName);
+		if (!command) {
+			console.error(`[ERROR] No se encuentra un comando llamado ${interaction.commandName}`);
+			return;
+		}
+		try {
+			console.log(`Comando recibido: ${interaction.commandName}`);
+			await command.execute(interaction);
+		}
+		catch (error) {
+			console.error(error);
+			await interaction.reply({ content: 'Mielda menol, no entendí el beta!', ephemeral: true });
+		}
+	},
+};
+
+module.exports = interactionCreate;
--- src/index.js
+++ src/index.js
@@ -13,52 +13,29 @@ const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // Register events
-for(const file of eventFiles) {
-  const filePath = path.join(eventsPath, file);
-  const event = require(filePath);
-  const handler = (...args) => event.execute(...args);
-  if(event.once) {
-    client.once(event.name, handler);
-  } else {
-    client.on(event.name, handler);
-  }
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	const handler = (...args) => event.execute(...args);
+	if (event.once) {
+		client.once(event.name, handler);
+	}
+	else {
+		client.on(event.name, handler);
+	}
 }
 
 
 // Register commands
 client.commands = new Collection();
-for(const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  if('data' in command && 'execute' in command) {
-    client.commands.set(command.data.name, command);
-    continue;
-  }
-  console.log(`[WARNING] El comando en ${filePath} no contiene los campos "data" o "execute"`);
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+		continue;
+	}
+	console.log(`[WARNING] El comando en ${filePath} no contiene los campos "data" o "execute"`);
 }
-
-// Interactions
-// client.on(Events.InteractionCreate, async interaction => {
-//   if(!interaction.isChatInputCommand()) {
-//     return;
-//   }
-//   const command = interaction.client.commands.get(interaction.commandName);
-//   if(!command) {
-//     console.error(`[ERROR] No se encuentra un comando llamado ${interaction.commandName}`);
-//     return;
-//   }
-//   try {
-//     console.log(`Comando recibido: ${interaction.commandName}`);
-//     await command.execute(interaction);
-//   } catch (error) {
-//     console.error(error);
-//     await interaction.reply({ content: 'Mielda menol, no entendí el beta!', ephemeral: true });
-//   }
-// });
-
-
-// client.once(Events.ClientReady, c => {
-// 	console.log(`Ready! Logged in as ${c.user.tag}`);
-// });
 
 client.login(botToken);
